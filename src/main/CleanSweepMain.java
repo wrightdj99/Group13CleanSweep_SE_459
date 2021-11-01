6ec0c012b701962c63f6c5961c3738b3363b7345
Colin Kieny <ckieny@depaul.edu>
2021-11-01 16:04:51 -0500

    Account.java added and login/register on terminal is working but doesn't save

--- src/main/CleanSweepMain.java
+++ src/main/CleanSweepMain.java
@@ -5,6 +5,9 @@ import java.util.*;
 
 public class CleanSweepMain {
 
+    private static final ArrayList<main.Account> accountList = new ArrayList<>(); // creating list of accounts.
+    private static Scanner scanner = new Scanner(System.in); // used to read user input
+
     public static RoomNode floor_master; // This is the master floor plan.
     public static ArrayList<RoomNode> floor_master_list; // This is a list containing all nodes in the floor plan.
     // The master floor plan exists as a mock for testing purposes only.
@@ -12,6 +15,9 @@ public class CleanSweepMain {
 
     // The control system's entry point.
     public static void main(String[] args) {
+        // User login will happen first.
+        accountRegistrationOrLogin();
+
         // Run demo methods here.
         demo_1();
         demo_2();
@@ -19,6 +25,139 @@ public class CleanSweepMain {
         demo_4();
         UserInterface ui = new UserInterface();
         // . . . etc.
+    }
+
+    public static void accountRegistrationOrLogin(){
+        // asks the user if they have an existing account or if they would like to register a new account.
+        // It then calls either login() or register()
+
+        // creating accounts for testing purposes
+        accountList.add(0, new main.Account("colin@mail.com", "123")); // tmp user
+        accountList.add(0, new main.Account("aneri@mail.com", "123"));
+        accountList.add(0, new main.Account("brandon@mail.com", "123"));
+        accountList.add(0, new main.Account("daniel@mail.com", "123"));
+
+        // ask the user if they're returning users, AKA already registered an account
+        boolean invalidInput = true;
+        while (invalidInput) {
+            System.out.print("Returning user? (Y/N) ");
+            String yesOrNo = scanner.nextLine();
+            if (yesOrNo.equals("Y")) {
+                invalidInput = false;
+                login();
+            } else if (yesOrNo.equals("N")) {
+                invalidInput = false;
+                register();
+            } else {
+                System.out.print("Invalid input. Please enter 'Y' or 'N'\n");
+            }
+        }
+        scanner.close();
+    }
+
+    public static void register(){
+        // Asks users for their email, a password, and to confirm password. If password and
+        // confirmed password are the same, their account is created and added to accountList.
+
+        String email = null, password;
+
+        // Check if user input a string containing both '@' and '.com'
+        boolean invalidEmail = true;
+        while(invalidEmail) {
+            System.out.print("Enter Email: ");
+            email = scanner.nextLine().toLowerCase(Locale.ROOT);
+            if (email.contains("@") && email.contains(".")) { // not a good way to check for email, will update if we work with email functionality.
+                invalidEmail = false; // causes while loop to stop
+            } else {
+                System.out.print("Please enter valid email containing '@' and '.com'\n");
+            }
+        }
+
+        // User inputs a password and confirms password. Check if they're the same.
+        boolean passwordsDoNotMatch = true;
+
+        while (passwordsDoNotMatch) {
+
+            System.out.print("Create New Password: ");
+            password = scanner.nextLine();
+
+            System.out.print("Confirm Password: ");
+            String confirmPassword = scanner.nextLine();
+
+            if (password.equals(confirmPassword)) {
+                passwordsDoNotMatch = false; // causes while loop to stop.
+
+                // check if email is null somehow.
+                if (email != null) {
+                    accountList.add(new main.Account(email, password));
+                    System.out.print("\nAccount successfully created!\n\n");
+                } else {
+                    System.out.print("\nemail was null somehow\n\n");
+                }
+            } else {
+                System.out.print("Passwords do not match, try again.\n");
+            }
+        }
+
+    }
+
+    public static void login() {
+        // asks users to login with their email and password.
+        // Email must be connected to an account in accountList.
+        // Password must be associated with the specific account the email is connected to.
+        // --> doesn't handle separate accounts with the same email.
+        boolean invalidEmail = true;
+        boolean invalidPassword = true;
+
+        String email, password;
+        Integer pos = null; // used to check position in accountList. Updated using emailPosition(). If null, no account with that email exists.
+
+        // check if email exists in accountList and get position if it does exist.
+        while (invalidEmail) {
+            System.out.print("Enter Email: ");
+            email = scanner.nextLine();
+
+            pos = emailPosition(email);
+
+            if (pos == null) {
+                System.out.print("Email doesn't exist, try again.\n");
+            } else {
+                invalidEmail = false;
+            }
+        }
+
+        // user will enter the password associated with their email.
+        while (invalidPassword) {
+            System.out.print("Enter Password: ");
+            // check if password is correct
+            password = scanner.nextLine();
+            if (passwordCorrect(password, pos)) {
+                invalidPassword = false;
+                System.out.print("\nLogin successful!\n\n");
+            } else {
+                System.out.print("Wrong password, try again.\n");
+            }
+        }
+    }
+
+    public static Integer emailPosition(String email) {
+        int pos;
+        for (pos = 0; pos < accountList.size(); pos++) {
+            main.Account a = accountList.get(pos);
+            if (a.getEmail().equals(email)){
+                return pos;
+            }
+        }
+        return null;
+    }
+
+    public static boolean passwordCorrect(String password, int pos){
+        main.Account a = accountList.get(pos);
+        if (password.equals(a.getPassword()))
+            return true;
+
+        return false;
+
     }
 
     public static void demo_1() {
