72864735c74607d2a4020c876837b031cd902e56
AneriMadhu <anerimadhu@gmail.com>
2021-11-14 15:37:07 -0800

    loginfo class added

---
+++ src/main/LogInfo.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.time.LocalDate;
+
+public class LogInfo {
+    private LocalDate currTime;
+    private Direction direction;
+    private
+}
+
+• The current date and time
+        • Position of unit
+        • Direction of travel
+        • Surface detected
+        • Cleaning status
+        • Dirt detected
+        • Obstacles detected
+        • Current power
