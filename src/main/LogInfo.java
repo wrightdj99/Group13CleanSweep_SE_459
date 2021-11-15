791ef48a19f30a6a4f00ac1763b7115b47ca48e8
AneriMadhu <anerimadhu@gmail.com>
2021-11-14 16:49:34 -0800

    Logging information added

--- src/main/LogInfo.java
+++ src/main/LogInfo.java
@@ -3,16 +3,46 @@ package main;
 import java.time.LocalDate;
 
 public class LogInfo {
+    //This class is used to keep track of all the logging information
     private LocalDate currTime;
-    private Direction direction;
-    private
-}
+    private Vector2 position;
+    private FloorType floorType;
+    private boolean isObstacle;
+    private boolean isOnReturnPath;
+    private float currCharge;
 
-• The current date and time
-        • Position of unit
-        • Direction of travel
-        • Surface detected
-        • Cleaning status
-        • Dirt detected
-        • Obstacles detected
-        • Current power
+    public LogInfo(LocalDate currTime,
+                   Vector2 position, FloorType floorType,
+                   boolean isObstacle, boolean isOnReturnPath, float currCharge){
+        this.currTime = currTime;
+        this.position = position;
+        this.floorType = floorType;
+        this.isObstacle = isObstacle;
+        this.isOnReturnPath = isOnReturnPath;
+        this.currCharge = currCharge;
+    }
+
+    public LocalDate getCurrTime(){
+        return this.currTime;
+    }
+
+    public Vector2 getPosition(){
+        return this.position;
+    }
+
+    public FloorType getFloorType(){
+        return this.floorType;
+    }
+
+    public boolean getIsObstacle(){
+        return this.isObstacle;
+    }
+
+    public boolean getIsOnReturnPath(){
+        return this.isOnReturnPath;
+    }
+
+    public float getCurrCharge(){
+        return this.currCharge;
+    }
+}
